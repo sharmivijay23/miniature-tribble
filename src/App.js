a0b8adb1b3dd3eb006fc16912168ebf36ae375c0
Sharmi Vijay <sharmivijay@Sharmis-MacBook-Pro.local>
2023-10-08 04:05:19 -0700

    adding items

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import * as React from 'react'
+import { ITEMS } from './data'
 import './App.css';
 
 function App() {
+
+  const [searchText, setSearchText] = React.useState("")
+
+  const inputChangeHandler = function (event) {
+    setSearchText(event.target.value)
+  }
+
+  const items = ITEMS.filter(item => item.en_US.toLowerCase().includes(searchText.toLowerCase()))
+
+  console.log("rendering App")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Items details</h1>
+      <input type="text" onChange={inputChangeHandler} value={searchText} placeholder="Search items"/>
+      <table>
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>English</th>
+            <th>Tamil</th>
+            <th>Hindi</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map(item => <MemoizedItem key={item.image} {...item} />)}
+        </tbody>
+      </table>
     </div>
   );
 }
 
+const MemoizedItem = React.memo(Item);
+
+function Item(props) {
+  console.log("rendering item")
+  return (
+    <tr>
+      <td>
+        <img src={props.image} width="100" height="100" />
+      </td>
+      <td>
+        {props.en_US}
+      </td>
+      <td>
+        {props.ta_IN}
+      </td>
+      <td>
+        {props.hi_IN}
+      </td>
+      
+    </tr>
+  )
+}
+
 export default App;
+
+// React.useState()
+// function Counter() {
+//   const [count, setCount] = React.useState(0)
+  
+//   const clickHandler = function (event) {
+//     setCount(count + 1)
+//   }
+
+//   console.log("rendering counter")
+//   return (
+//     <div>
+//       <button onClick={clickHandler}>Click Me</button>
+//       <div>I was clicked {count} times</div>
+//     </div>
+//   )
+// }
---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const ITEMS = [
+    {
+        en_US: "Tomato",
+        ta_IN: "தக்காளி",
+        hi_IN:"टमाटर",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Tomato_je.jpg/440px-Tomato_je.jpg",
+    },
+    {
+        en_US: "Potato",
+        ta_IN: "உருளைக்கிழங்கு",
+        hi_IN: "आलू",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Patates.jpg/440px-Patates.jpg",
+    },
+    {
+        en_US: "Carrot",
+        ta_IN: "கேரட்",
+        hi_IN: "गाजर",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Carrots_at_Ljubljana_Central_Market.JPG/440px-Carrots_at_Ljubljana_Central_Market.JPG"
+    }
+]
